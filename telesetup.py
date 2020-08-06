40dc6ca7e0c44762c1fbfc71a76f704c6361868a
Deepak15011 <67235095+Deepak15011@users.noreply.github.com>
2020-08-06 22:38:20 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -4,8 +4,8 @@ print ("")
 print ("")
 print("""processing.......""")
 
-API_KEY = '1754367'
-API_HASH = "231b8cc6cca12ee51a85cf543321f476"
+API_KEY = ''
+API_HASH = ""
 while True:
   try:
    with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
